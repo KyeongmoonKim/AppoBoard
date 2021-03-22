45ef7ac251cdd5be8b5d22d135f849208f7cd379
KyeongmoonKim <kkm8031@snu.ac.kr>
2021-03-22 20:53:18 +0900

    클래스 추가중

---
+++ src/config/UserConfig.java
@@ -0,0 +1,38 @@
+package config;
+import java.UserDAO;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.*;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.*;
+import org.springframework.transaction.annotation.*;
+//import javax.naming.*;
+//import javax.sql.*;
+
+@Configuration
+@EnableTransactionManagement
+public class UserConfig {
+	
+	@Bean(destroyMethod = "close") 
+	public DataSource dataSource(){ //DataSource Bean
+		DataSource ds = new DataSource();
+		ds.setDriverClassName("oracle.jdbc.OracleDriver"); //context.xml의 내용
+		ds.setUrl("jdbc:oracle:thin:@localhost:1521:XE"); //context.xml의내용
+		ds.setUsername("scott");
+		ds.setPassword("tiger");
+		ds.setInitialSize(2);
+		ds.setMaxActive(50); //max active 와 maxidle은 같은숫자로
+		ds.setMaxIdle(50);
+		return ds;
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		DataSourceTransactionManager tm = new DataSourceTransactionManager();
+		tm.setDataSource(dataSource());
+		return tm;
+	}
+	@Bean
+	public UserDAO userDao(){
+		return new UserDAO(dataSource());
+	}
+}
---
+++ src/java/UserDAO.java
@@ -0,0 +1,81 @@
+package java;
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.*;
+import org.springframework.jdbc.core.*;
+import org.springframework.transaction.annotation.*;
+
+public class UserDAO {
+	
+	private JdbcTemplate jdbcTemplate; 
+	public UserDAO(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	public void login(String id, String pwd) {
+		List<UserVO> results = jdbcTemplate.query(
+				"SELECT * FROM MYUSER WHERE USERID = ? ",
+				new RowMapper<UserVO>() {
+					@Override
+					public UserVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+						UserVO uvo = new UserVO();
+						uvo.setId(rs.getString("USERID"));
+						uvo.setPwd(rs.getString("USERPWD"));
+						uvo.setName(rs.getString("NAME"));
+						return uvo;
+					}
+				}, id);
+		//List<T> query(String sql, Object[] args, RowMapper<T> rowMapper) 같은 타입일경우에거
+		//List<T> query(String sql, RowMapper<T> rowMapper, Object... args)
+		if(results.size()!=1) throw new LoginFailException("no such ID"); //해당되는 id가 없거나 여러개임.
+		UserVO temp = results.get(0); //하나니까 가져옴.
+		if(temp.getPwd().compareTo(pwd)!=0) throw new LoginFailException("pwd is wrong!");
+	}
+	public boolean isDup(String id) {
+		List<UserVO> results = jdbcTemplate.query(
+				"SELECT * FROM MYUSER WHERE USERID = ? ",
+				new RowMapper<UserVO>() {
+					@Override
+					public UserVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+						UserVO uvo = new UserVO();
+						uvo.setId(rs.getString("USERID"));
+						uvo.setPwd(rs.getString("USERPWD"));
+						uvo.setName(rs.getString("NAME"));
+						return uvo;
+					}
+				}, id);
+		if(!results.isEmpty()) return true; //아이디 중복
+		else return false; //아이디 중복 없음
+	}
+	
+	public void insertUser(UserVO uvo) {
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				PreparedStatement pstmt = con.prepareStatement("INSERT INTO MYUSER(USERID, USERPWD, NAME) VALUES (?, ?, ?)");
+				pstmt.setString(1, uvo.getId());
+				pstmt.setString(2, uvo.getPwd());
+				pstmt.setString(3, uvo.getName());
+				return pstmt;
+			}
+		});
+	}
+	public void changePwd(String id, String pwd) {
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				PreparedStatement pstmt = con.prepareStatement("UPDATE MYUSER SET USERPWD = ? WHERE USERID = ?");
+				pstmt.setString(1, pwd);
+				pstmt.setString(2, id);
+				return pstmt;
+			}
+		});
+	}
+	@Transactional
+	public void changePwds(String[] ids, String[] pwds, String[] pwdNews) {
+		for(int i = 0; i < ids.length; i++) {
+			login(ids[i], pwds[i]); //login test;
+			changePwd(ids[i], pwdNews[i]);
+		}
+	}
+}
