6c92310d75b973fd1743aa421339d0fb17c42ed2
KyeongmoonKim <kkm8031@snu.ac.kr>
2021-03-29 20:02:29 +0900

    start sqlProcedure

--- WebContent/myjs/sqlProcedure.js
+++ WebContent/myjs/sqlProcedure.js
@@ -1,4 +1,21 @@
 /**
  * 
  */
+var jquery_script = document.createElement('script');
+jquery_script.src = '/AppoBoard/coco/jquery3.3.1.min.js';
+jquery_script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(jquery_script);
 
+function sqlProcedure() {
+	this.params = [];
+	this.count = 0;
+	this.addParams = function(key, value, type) {
+		var keytemp = 'key'+String(count);
+		var valuetemp = 'value'+String(count);
+		var typetemp = 'type'+String(count);
+		params[keytemp] = key;
+		params[valuetemp] = value;
+		params[typetemp] = type;
+	};
+	
+}
