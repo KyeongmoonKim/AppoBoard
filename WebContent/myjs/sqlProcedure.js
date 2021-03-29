2b2ef383f3acc730328353a0366e55b845217a86
KyeongmoonKim <kkm8031@snu.ac.kr>
2021-03-29 20:24:54 +0900

    client sqlProcedure finish, and server start

--- WebContent/myjs/sqlProcedure.js
+++ WebContent/myjs/sqlProcedure.js
@@ -7,15 +7,43 @@ jquery_script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(jquery_script);
 
 function sqlProcedure() {
-	this.params = [];
+	this.params = new Object();
 	this.count = 0;
 	this.addParams = function(key, value, type) {
 		var keytemp = 'key'+String(count);
 		var valuetemp = 'value'+String(count);
 		var typetemp = 'type'+String(count);
-		params[keytemp] = key;
-		params[valuetemp] = value;
-		params[typetemp] = type;
+		this.params[keytemp] = key;
+		this.params[valuetemp] = value;
+		this.params[typetemp] = type;
+		this.count = this.count+1;
 	};
 	
+	this.asyncAjax = function(sqlReq, call_back) {//sqlReq는 스트링, call_back은 콜백함수
+		var json_data = new Object();
+		json_data.sqlReq = sqlReq;
+		json_data.counts = String(this.count);
+		for(var i = 0; i < this.count; i++) {
+			var key0 = 'key'+String(count);
+			var key1 = 'value'+String(count);
+			var key2 = 'type'+String(count);
+			json_data[key0] = this.params[key0];
+			json_data[key1] = this.params[key1];
+			json_data[key2] = this.params[key2];
+		}
+		$.ajax({
+	         url: '/AppoBoard/Appointment/today',
+	         dataType: 'json',
+	         data: json_data,
+	         type: 'post',
+	         success: function(ret) { // check if available
+	           //success
+	           call_back(ret);
+	         },
+	         error: function() { // error logging
+	           console.log('Error!');
+	         }
+	       });
+		
+	}
 }
