a8e0c05a0084fc5fd3bae068e91ac7ba042378e3
KyeongmoonKim <kkm8031@snu.ac.kr>
2021-04-02 20:39:32 +0900

    일일일정 화면 시작

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -3,6 +3,9 @@ package controller;
 import user.*;
 import config.UserConfig;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.*;
@@ -30,7 +33,11 @@ public class UserController {
 		String ret = "";
 		try {
 			udao.login(id, pwd);
-			ret = "loginO";
+			long systemTime = System.currentTimeMillis();
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.KOREA);
+			// format에 맞게 출력하기 위한 문자열 변환
+			String dTime = formatter.format(systemTime);
+			ret = "todayAppo";
 		} catch(LoginFailException e) {
 			ret = "loginX";
 		}
