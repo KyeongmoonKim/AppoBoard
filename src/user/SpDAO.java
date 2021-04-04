3169183282669278c168defb91883a21d357f5dd
KyeongmoonKim <kkm8031@snu.ac.kr>
2021-04-04 14:20:02 +0900

    SpDAO 작어붖ㅇ

--- src/user/SpDAO.java
+++ src/user/SpDAO.java
@@ -15,14 +15,32 @@ public class SpDAO{
 	 }
 	 
 	 public Map<String, Object> exec(Map<String, Object> params) {
-		 SimpleJdbcCall jdbcCall = new SimpleJdbcCall(ds);
-		    Map<String, Object> result =
-		        jdbcCall.withProcedureName("PROC_NAME")
+		 int count = Integer.parseInt((String)params.get("counts"));
+		 Map<String, Object> values = new HashMap<String, Object>();
+		 SimpleJdbcCall jdbcCall = new SimpleJdbcCall(ds).withProcedureName((String)params.get("sqlReq"));
+		 for(int i = 0; i < count; i++) { //타입에 따라 넣을거임
+			 String pKey = (String)params.get((String)params.get("key"+Integer.toString(i)));
+			 String pValue = (String)params.get((String)params.get("value"+Integer.toString(i)));
+			 String pType = (String)params.get((String)params.get("type"+Integer.toString(i)));
+			 if(pType.compareTo("int")==0) {
+				 jdbcCall.addDeclaredParameter(new SqlParameter(pKey, Types.INTEGER));
+				 values.put(pKey, Integer.getInteger(pValue));
+			 } else if(pType.compareTo("int")==0) {
+				 jdbcCall.addDeclaredParameter(new SqlParameter(pKey, Types.VARCHAR));
+				 values.put(pKey, pValue);
+			 } else {
+				 System.out.println("not declared type in the spDAO:line28");
+				 throw new Error("not declared type in the spDAO:line28");
+			 }
+		 }
+		    /*Map<String, Object> result =
+		        jdbcCall.withProcedureName((String)params.get("sqlReq"))
 		            .declareParameters(
+		            		for(int i = 0; i < n; i++) {
 		                    new SqlParameter("param1", Types.VARCHAR),
-		                    new SqlParameter("param2", Types.VARCHAR),
+		            		}
 		                    new SqlOutParameter("results_cursor", OracleTypes.CURSOR, new SomeRowMapper()))
-		            .execute("some string", "some other string");
+		            .execute("some string", "some other string");*/
 	 }
 
 }
